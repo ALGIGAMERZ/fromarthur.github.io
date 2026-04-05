f61b314a2f34fd17d8dd50bbf490ab8e474b74dd
Arthur Mavindra <76931958+ALGIGAMERZ@users.noreply.github.com>
2026-04-05 20:29:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'A small space I made for you, Aily.',//untuk deskripsi planet layer 1
+  "I don't say much, but I stay.",//untuk deskripsi planet layer 2
+  "♡You make ordinary things feel different♡",//untuk deskripsi planet layer 3
+  "Arthur — for Aily",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
